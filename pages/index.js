095666da7746389b24f62c12af1d224a37f7e86a
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-01-07 12:58:17 +0100

    jo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function HomePage() {
+/* export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+    </div>
+  );
+}
+ */
+
+import Link from "next/link";
+import { introduction } from "../resources/lib/data.js";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <Link href="./volumes">Volumes</Link>
     </div>
   );
 }
